9c017c7264b0f2b12602e1503321156050b8fa8e
Sasha Koss <kossnocorp@gmail.com>
2011-05-20 09:56:08 +0700

    Added mechanism to pick element by id

--- jquery.role.js
+++ jquery.role.js
@@ -1,12 +1,23 @@
 /*
     jquery.role.js
 
+    Version: 0.2.0
+
     Copyright (c) 2011 Sasha Koss
 */
 
 (function ($) {
     $.role = function (roleName, context) {
-        return $('[role="' + roleName + '"]', context);
+
+        var selector;
+
+        if ( roleName.charAt(0) === '#' ) {
+            selector = '#role-' + roleName.substring(1);
+        } else {
+            selector = '[role="' + roleName + '"]';
+        }
+
+        return $(selector, context);
     };
 
     if (typeof $.r === 'undefined') {
